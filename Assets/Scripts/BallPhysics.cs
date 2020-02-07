2a75fc7a881fc2485f27704ebc8a0f0dba9b5758
Grib04eg <li4niykabinet@gmail.com>
2020-02-08 04:55:37 +0700

    First commit
    First commit

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Vector3 direction;
+    private Rigidbody2D rb;
+
+    private bool running;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (running)
+            rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
+    }
+
+    public void Run(Vector3 startDirection)
+    {
+        direction = startDirection.normalized;
+        running = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        direction = Vector2.Reflect(direction, collision.contacts[0].normal);
+        if (collision.contactCount > 1)
+        {
+            for (int i = 1; i < collision.contactCount; i++)
+            {
+                if (collision.contacts[i].normal != collision.contacts[0].normal)
+                    direction = Vector2.Reflect(direction, collision.contacts[i].normal);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] LineRenderer line;
+    [SerializeField] BallPhysics ball;
+
+    private bool running = false;
+    void Start()
+    {
+        line.SetPosition(0, ball.transform.position);
+    }
+
+    void Update()
+    {
+        if (!running)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ball.Run((Input.mousePosition - Camera.main.WorldToScreenPoint(ball.transform.position + Vector3.forward * Camera.main.transform.position.z)).normalized);
+                running = true;
+                line.gameObject.SetActive(false);
+            } else
+            {
+                line.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
